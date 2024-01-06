70586fd7398da5c91f65e4c137e00f52486b0e82
jay candel <133104319+jaylikesbunda@users.noreply.github.com>
2024-01-06 17:16:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
-    var isSeeking = false;
-    var currentTrackIndex = 0;
-    var trackList = [
-        { 
-          name: "FuckYou FM",
-          file: "https://audio.jukehost.co.uk/jdN2TbTn5tl8LDLklLVcpGjtPzkFfpRx",
-          image: "https://lh3.googleusercontent.com/9AKnVhbmPuNCPRaV_MfYjTIxqW5XJwJ39qLG3PRB7Yim6loJICpvw3_jK5tdPmtLTelqRmhfY2OUX0Q-HKdF3a3tUTOftzn83uXRD6PeDIZh6nZuris1E4GAiny1UoFWVg=w1280"
-        },
-        { 
-          name: "FuckYouFM: UTOPIA",
-          file: "https://audio.jukehost.co.uk/SmFrnGF37HsQntp8DW74RAG1LYJboFHy",
-          image: "https://lh5.googleusercontent.com/tBLFarPKk8cgabB-gvN6xEZRYv_8TYA_Wp5FyudPCLk7GBA1PC_IbtXIV6KVsIYiRhSpHd_haxlGimlgbEJvZ0H69dxqnU5tpKCsXFkTSUYaN8TMpnAvDSR7yPNLOCkq3w=w1280"
-        },
-        // Add more tracks as needed
-    ];
+  var isSeeking = false;
+  var currentTrackIndex = 0;
+  var trackList = [
+    { 
+      name: "FuckYou FM",
+      file: "https://audio.jukehost.co.uk/jdN2TbTn5tl8LDLklLVcpGjtPzkFfpRx",
+      image: "https://lh3.googleusercontent.com/9AKnVhbmPuNCPRaV_MfYjTIxqW5XJwJ39qLG3PRB7Yim6loJICpvw3_jK5tdPmtLTelqRmhfY2OUX0Q-HKdF3a3tUTOftzn83uXRD6PeDIZh6nZuris1E4GAiny1UoFWVg=w1280"
+    },
+    { 
+      name: "FuckYouFM: UTOPIA",
+      file: "https://audio.jukehost.co.uk/SmFrnGF37HsQntp8DW74RAG1LYJboFHy",
+      image: "https://lh5.googleusercontent.com/tBLFarPKk8cgabB-gvN6xEZRYv_8TYA_Wp5FyudPCLk7GBA1PC_IbtXIV6KVsIYiRhSpHd_haxlGimlgbEJvZ0H69dxqnU5tpKCsXFkTSUYaN8TMpnAvDSR7yPNLOCkq3w=w1280"
+    },
+    // Add more tracks with their respective images as needed
+  ];
 
-    // Function to populate the track list in the UI
     function populateTrackList() {
         var trackSelection = $('#track-selection');
         trackList.forEach(function(track, index) {
             $('<a>', {
                 'class': 'track-item',
                 'href': '#',
-                'data-index': index,
+                'data-src': track.file,
+                'data-image': track.image,
                 'text': track.name,
                 'click': function(e) {
                     e.preventDefault();
@@ -32,10 +32,9 @@ $(document).ready(function() {
         });
     }
 
-    // Function to initialize the jPlayer
     function initializePlayer() {
         $("#jquery_jplayer_1").jPlayer({
-            ready: function () {
+            ready: function() {
                 selectTrack(0);
             },
             swfPath: "/js",
@@ -46,27 +45,55 @@ $(document).ready(function() {
             smoothPlayBar: true,
             keyEnabled: true,
             remainingDuration: true,
-            toggleDuration: true
-        }).bind($.jPlayer.event.timeupdate, function(event) {
-            if (!isSeeking) {
-                var percentage = (event.jPlayer.status.currentTime / event.jPlayer.status.duration) * 100;
-                $(".jp-play-bar").css("width", percentage + "%");
+            toggleDuration: true,
+            timeupdate: function(event) {
+                if (!isSeeking) {
+                    updateSeekBar(event.jPlayer.status.currentTime, event.jPlayer.status.duration);
+                }
             }
         });
 
-        // Seeking functionality
-        $(".jp-progress").on("mousedown", function(e) {
+        $('.jp-progress').mousedown(function(e) {
             isSeeking = true;
-            updateSeekPosition(e.pageX);
-        }).on("mousemove", function(e) {
+            updateSeekBarPosition(e.pageX);
+        }).mousemove(function(e) {
             if (isSeeking) {
-                updateSeekPosition(e.pageX);
+                updateSeekBarPosition(e.pageX);
             }
-        }).on("mouseup", function() {
+        }).mouseup(function(e) {
+            if (isSeeking) {
+                isSeeking = false;
+                updateSeekBarPosition(e.pageX);
+            }
+        }).on('touchstart', function(e) {
+            e.preventDefault();
+            isSeeking = true;
+            updateSeekBarPosition(e.originalEvent.touches[0].pageX);
+        }).on('touchmove', function(e) {
+            if (isSeeking) {
+                updateSeekBarPosition(e.originalEvent.touches[0].pageX);
+            }
+        }).on('touchend', function() {
             isSeeking = false;
         });
 
-        // Adjust player size on window resize
+        // Volume control click
+        $('.jp-volume-bar').click(function(e) {
+            var volumeLevel = e.pageX - $(this).offset().left;
+            var volumePercentage = volumeLevel / $(this).width();
+            $("#jquery_jplayer_1").jPlayer("volume", volumePercentage);
+        });
+
+        // Mute and unmute controls
+        $('.jp-mute').click(function() {
+            $("#jquery_jplayer_1").jPlayer("mute");
+        });
+
+        $('.jp-unmute').click(function() {
+            $("#jquery_jplayer_1").jPlayer("unmute");
+        });
+
+        // Responsive resize
         $(window).resize(function() {
             $("#jquery_jplayer_1").jPlayer("option", "size", {
                 width: "100%",
@@ -75,21 +102,17 @@ $(document).ready(function() {
         });
     }
 
-    // Function to handle track selection
     function selectTrack(index) {
         currentTrackIndex = index;
         var track = trackList[index];
         $("#jquery_jplayer_1").jPlayer("setMedia", {
             mp3: track.file
         }).jPlayer("play");
-
         $("#header-image").attr("src", track.image);
         updateMediaSession(track.name);
-
         $('.track-item').removeClass('playing').eq(index).addClass('playing');
     }
 
-    // Function to update the media session
     function updateMediaSession(trackName) {
         if ('mediaSession' in navigator) {
             navigator.mediaSession.metadata = new MediaMetadata({
@@ -98,33 +121,30 @@ $(document).ready(function() {
         }
     }
 
-    // Function to update the seek position
-    function updateSeekPosition(pageX) {
+    function updateSeekBar(currentTime, duration) {
+        var percentage = (currentTime / duration) * 100;
+        $(".jp-play-bar").css("width", percentage + "%");
+        // Optional: Display current time and duration
+        $(".current-time").text(formatTime(currentTime));
+        $(".duration").text(formatTime(duration));
+    }
+
+    function updateSeekBarPosition(pageX) {
         var progressContainer = $(".jp-progress");
-        var position = pageX - progressContainer.offset().left;
-        var width = progressContainer.width();
-        var percentage = (position / width) * 100;
+        var progressBarOffset = progressContainer.offset().left;
+        var progressBarWidth = progressContainer.width();
+        var position = pageX - progressBarOffset;
+        var percentage = (position / progressBarWidth) * 100;
         $(".jp-play-bar").css("width", percentage + "%");
         $("#jquery_jplayer_1").jPlayer("playHead", percentage);
     }
 
-    // Volume control
-    $('.jp-volume-bar').click(function(e) {
-        var volumeLevel = e.pageX - $(this).offset().left;
-        var volumePercentage = volumeLevel / $(this).width();
-        $("#jquery_jplayer_1").jPlayer("volume", volumePercentage);
-    });
+    function formatTime(seconds) {
+        var minutes = Math.floor(seconds / 60);
+        seconds = Math.floor(seconds % 60);
+        return (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    }
 
-    // Mute and unmute controls
-    $('.jp-mute').click(function() {
-        $("#jquery_jplayer_1").jPlayer("mute");
-    });
-
-    $('.jp-unmute').click(function() {
-        $("#jquery_jplayer_1").jPlayer("unmute");
-    });
-
-    // Initialize the track list and player
     populateTrackList();
     initializePlayer();
 });
