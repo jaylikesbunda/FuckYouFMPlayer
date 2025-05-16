e0c393c523fda68d1c2ca126c333ea2489100145
jay candel <133104319+jaylikesbunda@users.noreply.github.com>
2025-05-16 08:31:26 +0800

    change caching

--- service-worker.js
+++ service-worker.js
@@ -24,28 +24,24 @@ self.addEventListener('install', event => {
 // Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
+    fetch(event.request).then(
+      response => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
-        return fetch(event.request).then(
-          response => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            // Clone the response
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
+        // Clone the response
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+        return response;
+      }
+    ).catch(() => {
+      // Network request failed, try to get it from the cache.
+      return caches.match(event.request);
+    })
   );
 });
 
